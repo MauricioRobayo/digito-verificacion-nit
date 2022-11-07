6dccbd8edf8f83995c202ac1bc7030fd4714aa60
Mauricio Robayo <rfmaj@gmail.com>
2022-11-06 22:38:00 -0500

    remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
 import { getVerificationDigit } from "nit-verifier";
-import { ChangeEvent, useMemo, useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 const formatter = new Intl.NumberFormat("es-CO", { useGrouping: true });
 
